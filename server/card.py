fc05ab1e25bd992211262661f0d4f6372048090b
Priyank Chaudhary <5903604+cpriyank@users.noreply.github.com>
2020-04-19 18:05:06 -0700

    card from text repr

--- server/card.py
+++ server/card.py
@@ -1,7 +1,19 @@
+from suit import Suit
+from rank import Rank
+
 class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+
+    @staticmethod
+    def from_text(text):
+        text = text.strip()
+        suit_value, rank_value = text[0], text[1]
+        # TODO: how inefficient is this?
+        suit = next(filter(lambda a_suit: a_suit.value == suit_value, Suit))
+        rank = next(filter(lambda a_rank: a_rank.value == rank_value, Rank))
+        return Card(suit, rank)
 
     def __eq__(self, other):
         if not isinstance(other, Card):
@@ -13,4 +25,7 @@ class Card:
         return hash(self.__repr__())
 
     def __repr__(self):
+        return f'{self.suit.value}{self.rank.value}'
+
+    def __str__(self):
         return f'{self.suit.value}{self.rank.value}'
--- server/card_test.py
+++ server/card_test.py
@@ -7,6 +7,11 @@ class TestCardMethods(unittest.TestCase):
     def test_equality(self):
         self.assertEqual(Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.ACE))
 
+    def test_card_from_text(self):
+        text = Suit.HEART.value + Rank.ACE.value
+        card = Card.from_text(text)
+        self.assertEqual(card, Card(Suit.HEART, Rank.ACE))
+
 if __name__ == "__main__":
     unittest.main()
 
