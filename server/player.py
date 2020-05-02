3a7efd0ef366508b52e639316d799d66ae2e49bc
Priyank Chaudhary <5903604+cpriyank@users.noreply.github.com>
2020-05-01 19:41:41 -0700

    jsonify helpers

--- server/player.py
+++ server/player.py
@@ -1,12 +1,20 @@
 import uuid
 
+
 class Player:
-    def __init__(self, name=''):
+    def __init__(self, name="", id=None, card_added=None, cards=None, cards_won=None):
+        if cards_won is None:
+            cards_won = []
+        if cards is None:
+            cards = []
         self.name = name
-        self.id = uuid.uuid4().hex
-        self.card_added = None
-        self.cards = []
-        self.card_won = []
+        if id:
+            self.id = id
+        else:
+            self.id = uuid.uuid4().hex
+        self.card_added = card_added
+        self.cards = cards
+        self.cards_won = cards_won
         # TODO: Add point calculation logic
         # self.points = 0
 
@@ -18,10 +26,10 @@ class Player:
 
     # required to make a type hashable to create a set, dict, etc out of it.
     def __hash__(self):
-        return hash(f'{self.name}{self.id}')
+        return hash(f"{self.name}{self.id}")
 
     def __repr__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
     def initialize_cards(self, initial_cards):
         self.cards = initial_cards
@@ -33,8 +41,10 @@ class Player:
     def undo_put_card(self, card):
         self.cards.append(card)
 
+    @property
+    def dict_form(self):
+        return vars(self)
+
     def add_cards_to_cards_won(self, cards):
-        self.card_won.extend(cards)
+        self.cards_won.extend(cards)
         # TODO: Update points
-
-
--- server/player_test.py
+++ server/player_test.py
@@ -1,10 +1,15 @@
 import unittest
 from player import Player
 from card import Card
+import json
+
 
 class TestDeckMethods(unittest.TestCase):
     def setUp(self):
-        self.a_player = Player("sweet")
+        self.player_1_name = "sweet"
+        self.player_2_name = "sour"
+        self.a_player = Player(self.player_1_name)
+        self.another_player = Player(self.player_2_name)
 
     def test_initialization(self):
         card = Card("heart", 1)
@@ -17,6 +22,17 @@ class TestDeckMethods(unittest.TestCase):
         self.a_player.put_card(card)
         self.assertFalse(self.a_player.cards)
         self.assertEqual(self.a_player.card_added, card)
+
+    def test_serialization(self):
+        players = [self.a_player, self.another_player]
+        serialized = json.dumps(self.a_player)
+        self.assertTrue(json.loads(serialized)['name'] == self.player_1_name)
+        to_dict_list = {"players": json.dumps([player.dict_form for player in players])}
+        # print(to_dict_list['players'])
+        serialized_list = json.dumps(to_dict_list)
+        deserialized_list = json.loads(serialized_list)
+        print(deserialized_list['players'])
+
 
 if __name__ == "__main__":
     unittest.main()
