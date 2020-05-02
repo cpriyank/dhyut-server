520784a49bf4284670ccc1116370b17095e75992
Priyank Chaudhary <5903604+cpriyank@users.noreply.github.com>
2020-05-01 19:39:14 -0700

    better tests

--- server/card_test.py
+++ server/card_test.py
@@ -3,15 +3,16 @@ from suit import Suit
 from rank import Rank
 from card import Card
 
+
 class TestCardMethods(unittest.TestCase):
     def test_equality(self):
         self.assertEqual(Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.ACE))
 
     def test_card_from_text(self):
-        text = Suit.HEART.value + Rank.ACE.value
+        text = Suit.HEART.value + Rank.TEN.value
         card = Card.from_text(text)
-        self.assertEqual(card, Card(Suit.HEART, Rank.ACE))
+        self.assertEqual(card, Card(Suit.HEART, Rank.TEN))
+
 
 if __name__ == "__main__":
     unittest.main()
-
