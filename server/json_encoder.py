2a42e31628fa6ee5da22f97b484e4aac0f267af3
Priyank Chaudhary <5903604+cpriyank@users.noreply.github.com>
2020-05-01 19:41:06 -0700

    poor man\'s encoder

--- server/json_encoder.py
+++ server/json_encoder.py
@@ -1,6 +1,33 @@
 from flask.json import JSONEncoder
-from card import Card
+from flask import Flask, jsonify, request
 from player import Player
+from card import Card
+from suit import Suit
+from rank import Rank
+import json
+
+# configuration
+DEBUG = True
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+class CustomPlayerEncoder(json.JSONEncoder):
+    def default(self, obj):  # pylint: disable = E0202
+        if isinstance(obj, Card):
+            return str(obj)
+        if isinstance(obj, Player):
+            return {
+                "id": obj.id,
+                "name": obj.name,
+                "card_added": obj.card_added,
+                # TODO: this is a hack
+                "cards": obj.cards,
+                "cards_won": obj.cards_won,
+            }
+        return super(CustomPlayerEncoder, self).default(obj)
 
 
 class GameObjectsEncoder(JSONEncoder):
@@ -9,13 +36,37 @@ class GameObjectsEncoder(JSONEncoder):
         return list(map(str, cards))
 
     def default(self, obj):  # pylint: disable = E0202
+        if isinstance(obj, Card):
+            return str(obj)
         if isinstance(obj, Player):
             return {
                 "id": obj.id,
                 "name": obj.name,
                 "card_added": obj.card_added,
                 # TODO: this is a hack
-                "cards": GameObjectsEncoder.jsonify_cards(obj.cards),
-                "card_won": GameObjectsEncoder.jsonify_cards(obj.card_won),
+                "cards": obj.cards,
+                "cards_won": obj.cards_won,
             }
         return super(GameObjectsEncoder, self).default(obj)
+
+
+# TODO: Following is poor man's way to test the API response json
+app.json_encoder = GameObjectsEncoder
+player = Player("magan", 123, Card(Suit.CLUB, Rank.ACE),
+                [Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.ACE)],
+                [Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.ACE)])
+player2 = Player("madan", 1234, Card(Suit.CLUB, Rank.ACE),
+                 [Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.ACE)],
+                 [Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.ACE)])
+
+players = [player, player2]
+
+
+# sanity check route
+@app.route("/ping", methods=["GET"])
+def ping_pong():
+    return jsonify(players)
+
+
+if __name__ == '__main__':
+    app.run()
